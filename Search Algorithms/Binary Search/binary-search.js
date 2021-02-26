eea1deebbae8d5d8d355ca14b7b6ccc60b3e068d
SRIRAM238 <75298072+SRIRAM238@users.noreply.github.com>
2021-02-26 22:31:45 +0530

    updated binary-search js

--- Search Algorithms/Binary Search/binary-search.js
+++ Search Algorithms/Binary Search/binary-search.js
@@ -1,23 +1,18 @@
 function binarySearch(arr,value) {
 var left = 0;
 var right = (arr.length-1);
-
 var middle = Math.floor((left + right)/2);
 console.log(left,middle,right);
 while(arr[middle]!== value && (left <= right) )
 {
-  
-    
-          if(value > arr[middle])
+   if(value > arr[middle])
           {
               left = middle+1;
           } else if (value < arr[middle]) {
               right = middle-1;
           }
        middle = Math.floor((left + right)/2);
-       
 }
-
 if(arr[middle] === value)
        {
          return middle;  
